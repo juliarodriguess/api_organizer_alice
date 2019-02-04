7a6b663656a9746d9ace5bf8079f55bbe57f034c
Julia Rodrigues <srodriguesjulia@gmail.com>
2019-02-04 19:58:27 -0200

    Teste de referência de atividades na trilha

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const server = require('./config/server')
 require('./config/database')
 require('./config/routes')(server)
+
+const Activity = require('./api/activities/activities')
+const Road = require('./api/roads/roads')
+
+let futebol = new Activity({
+    title: 'Futebol'
+})
+
+futebol.save()
+
+
+let corpo = new Road({
+    title: 'Corpo',
+    activities:[{
+        informations: futebol._id
+    }]
+})
+
+corpo.save()
