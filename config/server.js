aa54648ab8074d8adecf82e4ae03dd8602007bbd
Julia Rodrigues <srodriguesjulia@gmail.com>
2019-02-06 22:43:54 -0200

    Incluir cors

--- config/server.js
+++ config/server.js
@@ -2,9 +2,11 @@ const port = 4000
 const bodyParser = require('body-parser')
 const express = require('express')
 const server = express()
+const cors = require('cors')
 
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
+server.use(cors())
 
 server.listen(process.env.PORT || port, function() {
     console.log('server running on port 4000')
