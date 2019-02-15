22a14c58b8e2014fece01ad6729a6c5ba09a170f
Julia Rodrigues <srodriguesjulia@gmail.com>
2019-02-15 21:53:55 -0200

    Incluir configurações de CORS

--- config/server.js
+++ config/server.js
@@ -7,6 +7,18 @@ const cors = require('cors')
 server.use(bodyParser.urlencoded({extended: true}))
 server.use(bodyParser.json())
 server.use(cors())
+server.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, x-access-token"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+  
+    next();
+  });
+  
 
 server.listen(process.env.PORT || port, function() {
     console.log('server running on port 4000')
