086c4fdd177def8a0712a597bf6f697227a5bbf7
Julia Rodrigues <srodriguesjulia@gmail.com>
2019-02-20 23:47:03 -0300

    Mudar informações no schema de Roads Priority

--- api/agenda/agenda.js
+++ api/agenda/agenda.js
@@ -4,10 +4,10 @@ const Schema = restful.mongoose.Schema
 const agendaSchema = new Schema({
     _id: { type: Schema.Types.ObjectId, auto: true },
     user: {
-        name: { type: String },
-        class: { type: String },
+        fullName: { type: String },
+        classRoom: { type: String },
     },
-    roadsPriority: { type: [Number]},
+    roadsPriority: { type: [String]},
     roadChosen: { type: Number }
 })
 
