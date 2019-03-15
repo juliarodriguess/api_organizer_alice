4631e477b09949348613679f778daa9db5208c5b
Julia Rodrigues <srodriguesjulia@gmail.com>
2019-03-15 01:05:16 -0300

    Transformar schema da presença em array de objetos

--- api/agenda/agenda.js
+++ api/agenda/agenda.js
@@ -13,13 +13,23 @@ const agendaSchema = new Schema({
         id: { type: String }
     }],
     roadChosen: { type: String },
-    attendance: {
-        monday: {type: Boolean},
-        tuesday: {type: Boolean},
-        wednesday: {type: Boolean},
-        thursday: {type: Boolean},
-        friday: {type: Boolean}
-    }
+    attendance: [
+        {
+            monday: {type: Boolean}
+        },
+        {
+            tuesday: {type: Boolean}
+        },
+        {
+            wednesday: {type: Boolean}
+        },
+        {
+            thursday: {type: Boolean}
+        },
+        {
+            friday: {type: Boolean}
+        }
+    ]
 })
 
 module.exports = restful.model('Agenda', agendaSchema)
