b12f5d70330443fd13f593c66a144b76910dd418
Julia Rodrigues <srodriguesjulia@gmail.com>
2019-03-31 22:05:08 -0300

    Remover updateOptions

--- api/agenda/agendaService.js
+++ api/agenda/agendaService.js
@@ -2,6 +2,6 @@ const Agenda = require('./agenda')
 
 Agenda.methods(['get', 'post', 'put','options', 'patch', 'delete'])
 
-Agenda.updateOptions({new:true, runValidators: true})
+// Agenda.updateOptions({new:true, runValidators: true})
 
 module.exports = Agenda
