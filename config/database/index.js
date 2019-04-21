d01ab97458dc9cb5162ce619541d566d5a06c705
Julia Rodrigues <srodriguesjulia@gmail.com>
2019-04-21 20:20:18 -0300

    Atualizar database

--- config/database/index.js
+++ config/database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/noderest', { useNewUrlParser: true });
+mongoose.connect('mongodb://admin:alice2018@ds219095.mlab.com:19095/organizerdb', { useNewUrlParser: true });
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
