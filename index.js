c51a53e890f4ac56380176634b39ad34e1533fbe
Julia Rodrigues <srodriguesjulia@gmail.com>
2019-04-22 08:29:35 -0300

    Adicionar configurações de cors

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.use(cors())
+app.use(function(req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, x-access-token"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+
+    next();
+  });
 
 require('./api/controllers/authController')(app);
 require('./api/controllers/agendaController')(app);
@@ -17,4 +30,6 @@ app.get('/', (req, res) => {
 });
 
 app.listen(process.env.PORT || 5000)
+
+
 
