ca58d1c82dac7c53959211caa02cbddd6834fbf0
Julia Rodrigues <srodriguesjulia@gmail.com>
2019-04-22 07:30:25 -0300

    Mudar direcionamento de arquivo

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-require('./app/controllers/authController')(app);
-require('./app/controllers/agendaController')(app);
+require('./api/controllers/authController')(app);
+require('./api/controllers/agendaController')(app);
 
 // app.get('/', (req, res) => {
 //     res.send('Hello World')
